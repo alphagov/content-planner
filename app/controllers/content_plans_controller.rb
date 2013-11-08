ef46c4d1f6c59799da0c672f76d2d758bab4aed9
Matthew Ford <matt@bitzesty.com>
2013-11-08 18:14:06 +0000

    content plan nav

--- app/controllers/content_plans_controller.rb
+++ app/controllers/content_plans_controller.rb
@@ -1,2 +1,8 @@
 class ContentPlansController < ApplicationController
+  expose(:content_plan, attributes: :content_plan_params)
+  expose(:content_plans)
+
+  def source_url_params
+    params.require(:content_plan).permit(:type, :size, :status, :ref_no, :title, :details, :slug, :content_type, :sources, :handover_detailed_guidance, :notes)
+  end
 end
