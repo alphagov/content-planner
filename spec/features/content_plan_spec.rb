0c238b045e8e369b78ccc303d4f304a97f2f6de4
Matthew Ford <matt@bitzesty.com>
2013-11-13 17:24:24 +0000

    update content plan spec

--- spec/features/content_plan_spec.rb
+++ spec/features/content_plan_spec.rb
@@ -11,11 +11,14 @@ feature "Content Plan management" do
     visit new_content_plan_path
 
     fill_in "Title", with: "Some title"
-    fill_in "Ref no", with: "X1"
+    fill_in "Ref no", with: "X"
     fill_in "Size", with: "1"
 
     click_button "Create Content plan"
 
     expect(page).to have_text("Content plan was successfully created.")
+    expect(page).to have_text("Some title")
+    expect(page).to have_text("X")
+    expect(page).to have_text("1")
   end
 end
