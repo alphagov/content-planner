efa25a9cd6b7aba34bf7a05f4a91e129dc3c564c
Matthew Ford <matt@bitzesty.com>
2013-11-22 12:34:30 +0000

    disable papertrail in tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ ContentPlanner::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.after_initialize do
+    PaperTrail.enabled = false
+  end
 end
