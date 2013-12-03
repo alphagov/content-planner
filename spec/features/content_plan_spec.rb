ac8e0f358b77d60d6d9d7e5106f5a5f76136c177
Matthew Ford <matt@bitzesty.com>
2013-12-03 18:02:55 +0000

    Update spec to remove size

--- spec/features/content_plan_spec.rb
+++ spec/features/content_plan_spec.rb
@@ -12,13 +12,11 @@ feature "Content Plan management" do
 
     fill_in "Title", with: "Some title"
     fill_in "Ref no", with: "X"
-    fill_in "Size", with: "1"
 
     click_button "Create Content plan"
 
     expect(page).to have_text("Content plan was successfully created.")
     expect(page).to have_text("Some title")
     expect(page).to have_text("X")
-    expect(page).to have_text("1")
   end
 end
