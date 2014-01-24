f2ee7ffd0f8e7008b11b26369884a92be8a847a3
Mario Andrés Correa <admin@macool.me>
2014-01-24 11:19:48 -0500

    fix spec

--- spec/features/add_content_to_plan_directly_from_plan_spec.rb
+++ spec/features/add_content_to_plan_directly_from_plan_spec.rb
@@ -6,10 +6,10 @@ feature "Add content to plan directly from the Content Plan" do
     user = build :user
     user.permissions = [User::Permissions::SIGNIN, User::Permissions::GDS_EDITOR]
     user.save
-    content_plan = create :content_plan
   end
 
   scenario "new content from Content Plan" do
+    content_plan = create :content_plan
     visit content_plan_path(content_plan)
     click_link "Add content"
     find("#content_content_plan_ids").value.should include(content_plan.id.to_s)
