f0d36b25d0f8552c23023b551ab97be19e563736
Matthew Ford <matt@bitzesty.com>
2014-01-24 14:08:59 +0000

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+def create_users
+  User.new { |u|
+    u.name = 'Winston'
+    u.uid = 'winston'
+    u.version = 1
+    u.email = 'winston@alphagov.co.uk'
+    u.permissions = ['signin', 'GDS Editor']
+  }.save
+end
+
+
+create_users
