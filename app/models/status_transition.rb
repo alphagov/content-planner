a938e217f21b70bb361ccf5da27f4f1c7db33bfa
Aaron Tian <Aaron2Ti@gmail.com>
2014-01-27 20:13:41 +0800

    apply the date filters for transitions of status

--- app/models/status_transition.rb
+++ app/models/status_transition.rb
@@ -10,4 +10,7 @@ class StatusTransition < ActiveRecord::Base
   scope :mainstream, -> { where content_id: Content.mainstream }
 
   scope :order_by_occurred_at_desc, -> { order occurred_at: :desc }
+
+  scope :start_after, ->(started_from) { where "occurred_at > ?",  started_from }
+  scope :end_before,  ->(ended_at)     { where "occurred_at <= ?", ended_at     }
 end
