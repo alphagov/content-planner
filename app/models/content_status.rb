cf1f92c8b236ab8f14a436d43ae6230e81638bd6
Aaron Tian <Aaron2Ti@gmail.com>
2014-01-27 15:18:15 +0800

    + ContentStatus

---
+++ app/models/content_status.rb
@@ -0,0 +1,21 @@
+class ContentStatus < ActiveHash::Base
+  self.data = [
+    { id: 'not_started', name: 'Not started' },
+    { id: 'in_progress', name: 'In progress' },
+    { id: 'completed',   name: 'Completed'   },
+    { id: 'published',   name: 'Published'   }
+  ]
+
+  NOT_STARTED = find 'not_started'
+  IN_PROGRESS = find 'in_progress'
+  COMPLETED   = find 'completed'
+  PUBLISHED   = find 'published'
+
+  include Comparable
+  #
+  # ContentStatus.find('not_started') < ContentStatus.find('in_progress') # true
+  #
+  def <=>(another)
+    self.class.all.index(self) <=> self.class.all.index(another)
+  end
+end
