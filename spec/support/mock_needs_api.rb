61a724a5d093030d8a195c80f37894b47c762301
Mario Andrés Correa <admin@macool.me>
2014-01-28 17:49:45 -0500

    stubbing need api

---
+++ spec/support/mock_needs_api.rb
@@ -0,0 +1,16 @@
+class MockNeedsApi
+  attr_reader :need
+
+  def initialize(*need)
+    @need = Need.new(need)
+  end
+  def needs
+    self
+  end
+  def with_subsequent_pages
+    []
+  end
+  def organisations
+    []
+  end
+end
