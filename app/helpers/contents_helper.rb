ea9d0f741d34aed03e454d95f92457ae7f528128
Matthew Ford <matt@bitzesty.com>
2014-02-10 14:59:58 +0000

    EndAlignment

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,5 +1,5 @@
 module ContentsHelper
-    def content_tag_filters(tags)
+  def content_tag_filters(tags)
     tags.map do |t|
       link_to(t, contents_path(search: {tag: t}), class: 'label label-success tag')
     end.join.html_safe
