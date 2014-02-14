adc4fb5afb68da473e3c2ad0ed5968db45d76de9
Matthew Ford <matt@bitzesty.com>
2014-02-14 14:34:18 +0000

    Fix specs

--- spec/mailers/comment_mailer_spec.rb
+++ spec/mailers/comment_mailer_spec.rb
@@ -7,16 +7,19 @@ describe CommentMailer do
                 create(:user),
                 create(:user)]
     end
+
     it "should deliver a notification to all users that belong to a content except the user that commented" do
       content = create :content
       @users.each do |user|
         content.content_users << ContentUser.create(user: user, content: content)
       end
+      content.reload
       Comment.create(commentable: content, user: @users.first)
       deliveries = ActionMailer::Base.deliveries
       deliveries.length.should eq(2)
       deliveries.map(&:to).flatten.should eq(@users.slice(1, 2).map(&:email))
     end
+    
     it "should deliver a notification to all users that belong to contents inside a content plan (except the user that commented)" do
       content_plan = create :content_plan
       @users.each do |user|
@@ -24,12 +27,14 @@ describe CommentMailer do
         ContentUser.create(user: user, content: content)
         ContentPlanContent.create(content: content, content_plan: content_plan)
       end
+      content_plan.reload
       Comment.create(commentable: content_plan, user: @users.first)
       deliveries = ActionMailer::Base.deliveries
       deliveries.length.should eq(2)
       deliveries.map(&:to).flatten.should eq(@users.slice(1, 2).map(&:email))
     end
   end
+  
   describe "email content" do
     let(:user)      { create :user }
     let(:commenter) { create :user }
