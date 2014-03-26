2696a048df0d3c910c3b3d06390d44fc582aa055
Mario Andrés Correa <a@macool.me>
2014-03-26 14:26:55 -0500

    enhance content_plan factory and add tags factory

--- spec/factories/content_plans.rb
+++ spec/factories/content_plans.rb
@@ -2,9 +2,32 @@
 
 FactoryGirl.define do
   factory :content_plan do
-    ref_no "MyString"
-    title "MyString"
-    details "MyText"
-    notes "MyText"
+    sequence(:ref_no) { |n| "REF-#{"%03d" % n}" }
+    sequence(:title)  { |n| "Content plan #{n}" }
+
+    details     { "#{title} details" }
+    notes       { "#{title} notes" }
+    due_quarter { ContentPlan::QUARTERS.to_a.sample }
+    due_year    { ContentPlan::YEARS.to_a.sample }
+
+    trait :with_organisation do
+      after(:create) do |content_plan|
+        content_plan.organisation_ids = [Organisation.all.first.id]
+        content_plan.save!
+      end
+    end
+
+    trait :with_needs do
+      after(:create) do |content_plan|
+        ContentPlanNeed.create(content_plan: content_plan, need_id: Need.all.first.id)
+      end
+    end
+
+    trait :with_tags do
+      after(:create) do |content_plan|
+        content_plan.tag_list.add create(:tag).name
+        content_plan.save!
+      end
+    end
   end
 end
