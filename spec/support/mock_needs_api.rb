f0fa875c0e68ca1e1603a04b0a48e7afd45f20a5
Mario Andrés Correa <a@macool.me>
2014-03-26 14:25:53 -0500

    enhance MockNeedsApi
    it mocks now one need by default

--- spec/support/mock_needs_api.rb
+++ spec/support/mock_needs_api.rb
@@ -9,7 +9,29 @@ class MockNeedsApi
     self
   end
 
+  def need(id)
+    with_subsequent_pages.find { |n| n.id == id }
+  end
+
+  def organisation
+    OpenStruct.new(id: "hmrc",
+                   name: "HM Revenue & Customs",
+                   govuk_status: "live",
+                   abbreviation: "HMRC",
+                   parent_ids: [],
+                   child_ids: [])
+  end
+
   def with_subsequent_pages
-    []
+    [
+      OpenStruct.new(id: 100252,
+                     role: "user",
+                     goal: "go to the university",
+                     benefit: "I can graduate",
+                     organisation_ids: ["hmrc"],
+                     organisations: [organisation],
+                     applies_to_all_organisations: false,
+                     in_scope: nil)
+    ]
   end
 end
