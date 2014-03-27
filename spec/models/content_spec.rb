de7a7297737cbe9b4accc87d7e701615b552c2aa
Mario Andrés Correa <a@macool.me>
2014-03-26 21:39:50 -0500

    refactor validations

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,12 +1,14 @@
 require "spec_helper"
 
 describe Content do
-  it "should not be valid without a title" do
-    content = FactoryGirl.build :content, title: nil
-    content.valid?.should be_false
-    content.errors.messages[:title].should include("can't be blank")
-  end
-  it "should be valid with a title" do
-    FactoryGirl.build(:content).should be_valid
+  context "validations" do
+    describe "title" do
+      let(:content) { FactoryGirl.build(:content, title: nil) }
+
+      before { content.valid? }
+
+      it { should_not be_valid }
+      it { content.errors.messages[:title].should include("can't be blank") }
+    end
   end
 end
