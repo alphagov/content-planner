09c0b366f02c468541331ad4a36ac2399a9fc265
Mario Andrés Correa <a@macool.me>
2014-03-27 10:28:16 -0500

    factories for content

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -2,9 +2,14 @@
 
 FactoryGirl.define do
   factory :content do
-    title "Content Title"
-    url "MyText"
-    content_type "MyString"
-    status "MyString"
+    ref_no
+    sequence(:title)        { |n| "Content #{n}" }
+    sequence(:url)          { |n| "https://www.gov.uk/content/#{n}" }
+    sequence(:content_type) { |n| "Content type #{n}" }
+    sequence(:description)  { |n| "Description #{n}" }
+    platform                { Content::PLATFORMS.sample }
+    status                  { Content::STATUSES[platform].sample }
+    size                    { rand(10) + 1 }
+    publish_by              { Date.today }
   end
 end
