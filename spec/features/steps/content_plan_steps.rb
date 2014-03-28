ab798f958a2b96228d409ca066b981e6dbdb63ea
Mario Andrés Correa <a@macool.me>
2014-03-28 10:50:05 -0500

    refactor `content_plan_filter_spec` into `content_plan_spec`

--- spec/features/steps/content_plan_steps.rb
+++ spec/features/steps/content_plan_steps.rb
@@ -7,4 +7,15 @@ module ContentPlanSteps
     fill_in "Ref no", with: "X"
     click_button "Create Content plan"
   end
+
+  def fill_in_filter_form_for(content_plan)
+    fill_in "search_ref_no", with: content_plan.ref_no
+    fill_in "search_tag", with: content_plan.tags.first.to_s
+    select content_plan.organisations.first.to_s, from: "search_organisation_ids"
+    select content_plan.content_plan_needs.first.need.to_s, from: "search_need_id"
+    select content_plan.users.first.to_s, from: "search_user_id"
+    due_date = "q#{content_plan.due_quarter} #{content_plan.due_year}"
+    select due_date, from: "search_due_date"
+    click_on "Search"
+  end
 end
