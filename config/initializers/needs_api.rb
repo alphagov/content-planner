57aa0c5981872bdb4ce7c560b03fe253157f2909
Ruslan Khamidullin <rusllonrails@gmail.com>
2014-04-18 16:10:55 +0400

    Move API configuration into config/environments and mock in dev
    Allows the optional use of the APIs in dev with the USE_API flag e.g
    
    USE_API=true ./startup.sh

--- config/initializers/needs_api.rb
+++
@@ -1,4 +0,0 @@
-require 'gds_api/need_api'
-
-ContentPlanner.needs_api = GdsApi::NeedApi.new( Plek.current.find('need-api'), API_CLIENT_CREDENTIALS )
-
--- config/initializers/organisations_api.rb
+++
@@ -1,3 +0,0 @@
-require 'gds_api/organisations'
-
-ContentPlanner.organisations_api = GdsApi::Organisations.new( Plek.current.find('whitehall-admin') )
