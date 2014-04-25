e612834d8086b6acc02f226d326077577bfa0a24
RuslanHamidullin <ror_developer@mail.ru>
2014-04-25 20:44:04 +0400

    68637480 comments ability to edit and delete
    https://www.pivotaltracker.com/story/show/68637480
    
    - added base edit and update functionality
    - added comment permissions policy via pundit
    - added Rspec checks on edit and delete comment
    - added Rspec checks for permission rules:
      - check on comment owner
      - check on time passed (comment can be updated or deleted only in 5 minutes after posting)

---
+++ spec/support/click_on_helper.rb
@@ -0,0 +1,40 @@
+module ClickOnHelper
+  # Examples
+  #
+  #   click_on '.new_foo'   # <a href='#' class='new_foo'>foo</a>
+  #   click_on '#save_foo'  # <a href='#' id='save_foo'>foo</a>
+  #   click_on ':#save_foo' # <a href='#save_foo'>foo</a>
+  #   click_on '/some/url'  # <a href='/some/url'>foo</a>
+  #   click_on '+ New Foo'  # <a href='#'>+ New Foo</a>
+  #   click_on 'leave_feedback'  # <a href='#' data-action='leave_feedback'>Foo</a>
+  #
+  def click_on(locator)
+    el = case locator[0]
+         when ':'
+           val = locator[1..-1]
+
+           find %(a[href="#{val}"])
+
+         when '.', '#'
+           find locator
+
+         else
+           if locator =~ /\//
+            find %(a[href="#{locator}"])
+
+           else
+             find %(a[data-action="#{locator}"], input[data-action="#{locator}"])
+           end
+         end
+
+    if el
+      el.click
+
+    else
+      super locator
+    end
+
+  rescue Capybara::ElementNotFound, Nokogiri::CSS::SyntaxError
+    super locator
+  end
+end
