f89df5bc7a10d3975e0db962b9ac8399e2fb3174
RuslanHamidullin <ror_developer@mail.ru>
2014-05-02 20:30:35 +0400

    68355394 added import orgs to cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,10 @@ env :PATH, '/usr/local/bin:/usr/bin:/bin'
 # We need Rake to use our own environment (need to correct)
 job_type :rake, "cd :path && govuk_setenv content_planner bundle exec rake :task --silent :output"
 
+every :day, at: '6am' do
+  rake "organisations:import"
+end
+
 every :day, at: '8am' do
   rake "tasks:overdue_notifications"
 end
