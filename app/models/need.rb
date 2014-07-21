466112d74f00a75959176c434c3631888ba61506
Matthew Ford <matt@bitzesty.com>
2014-07-21 19:06:10 +0100

    Remove commented out code that is no longer needed
    These models are now persisted in the database

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,5 +1,3 @@
-# require "lrucache"
-
 class Need < ActiveRecord::Base
   has_many :organisation_needs, dependent: :destroy
   has_many :organisations, through: :organisation_needs
@@ -11,66 +9,6 @@ class Need < ActiveRecord::Base
   has_many :contents, through: :content_needs
 
   validates :api_id, presence: true
-
-  # cattr_writer :needs
-  # attr_accessor :data
-
-  # def self.cache
-  #   @cache ||= LRUCache.new(soft_ttl: 2.minutes, ttl: 10.minutes)
-  # end
-
-  # def self.reset_cache
-  #   @cache = nil
-  # end
-
-  # def self.cache_fetch(key)
-  #   inner_exception = nil
-  #   cache.fetch(key) do
-  #     begin
-  #       yield
-  #     rescue GdsApi::BaseError => e
-  #       inner_exception = e
-  #       raise RuntimeError.new("use_stale_value")
-  #     end
-  #   end
-  # rescue RuntimeError => e
-  #   if e.message == "use_stale_value"
-  #     raise inner_exception
-  #   else
-  #     raise
-  #   end
-  # end
-
-  # def self.all
-  #   cache_fetch("all") do
-  #     load_needs
-  #   end
-  # end
-
-  # def self.load_needs
-  #   ContentPlanner.needs_api.needs.with_subsequent_pages.map do |n|
-  #     new(n)
-  #   end.compact
-  # end
-
-  # def self.find(n)
-  #   cache_fetch("find_#{n}") do
-  #     new(ContentPlanner.needs_api.need(n))
-  #   end
-  # end
-
-  # def initialize(data)
-  #   @data = data
-  # end
-
-  # def ==(other)
-  #   other.is_a?(self.class) && other.id == id
-  # end
-
-  # TODO: need to check this (it should be api_id)
-  # def id
-  #   @data.id
-  # end
 
   def org_names
     organisations.map(&:abbreviation)
--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -10,57 +10,6 @@ class Organisation < ActiveRecord::Base
 
   validates :api_id, presence: true
 
-  # cattr_writer :organisations
-
-  # attr_reader :id, :title, :format, :slug, :abbreviation, :govuk_status, :parent_organisations, :logo_formatted_name, :organisation_brand_colour_class_name, :organisation_logo_type_class_name
-
-  # def self.cache
-  #   @cache ||= LRUCache.new(soft_ttl: 1.day, ttl: 2.days)
-  # end
-
-  # def self.reset_cache
-  #   @cache = nil
-  # end
-
-  # def self.cache_fetch(key)
-  #   inner_exception = nil
-  #   cache.fetch(key) do
-  #     begin
-  #       yield
-  #     rescue GdsApi::BaseError => e
-  #       inner_exception = e
-  #       raise RuntimeError.new("use_stale_value")
-  #     end
-  #   end
-  # rescue RuntimeError => e
-  #   if e.message == "use_stale_value"
-  #     raise inner_exception
-  #   else
-  #     raise
-  #   end
-  # end
-
-  # def initialize(org)
-  #   @id = org.details.slug #to preserve compatibility with old API
-  #   @title = org.title
-  #   @format = org.format
-  #   @updated_at = Time.parse(org.updated_at) if org.updated_at
-  #   @web_url = org.web_url
-  #   @slug = org.details.slug
-  #   @abbreviation = org.details.abbreviation
-  #   @closed_at = Time.parse(org.details.closed_at) if org.details.closed_at
-  #   @govuk_status = org.details.govuk_status
-  #   @parent_organisations = org.parent_organisations
-  #   @child_organisations = org.child_organisations
-  #   @logo_formatted_name = org.details.logo_formatted_name
-  #   @organisation_brand_colour_class_name = org.details.organisation_brand_colour_class_name
-  #   @organisation_logo_type_class_name = org.details.organisation_logo_type_class_name
-  # end
-
-  # def self.model_name
-  #   "Organisation"
-  # end
-
   def self.param_key
     "org"
   end
@@ -92,26 +41,4 @@ class Organisation < ActiveRecord::Base
   def path
     "/government/organisations/#{slug}"
   end
-
-  # def self.all
-  #   cache_fetch("all") do
-  #     load_organisations
-  #   end
-  # end
-
-  # def self.find(slug)
-  #   all.find { |organisation| organisation.slug == slug }
-  # end
-
-  private
-
-  # def self.load_organisations
-  #  (organisations_api.organisations.with_subsequent_pages || []).map { |org|
-  #     new org
-  #   }.reject { |dep| dep.exempt? }
-  # end
-
-  # def self.organisations_api
-  #   ContentPlanner.organisations_api
-  # end
 end
