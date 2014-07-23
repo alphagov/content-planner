a9ca9a9fd934495835705e18d9d04828d7376fc8
Mario Andrés Correa <a@macool.me>
2014-07-23 15:32:46 -0500

    platform wording amends
    things messed up in `20140421143815_platfrom_wording_changes`. Change wording:
    `whitehall -> publisher`
    `publisher -> whitehall`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517002643) do
+ActiveRecord::Schema.define(version: 20140723200657) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
